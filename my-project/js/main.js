e93fd8b0b3ec866e64f82f4a1627979593b5dd73
williamcorwin <williamc400@nycstudents.net>
2022-04-27 13:58:13 -0400

    1234

--- my-project/js/main.js
+++ my-project/js/main.js
@@ -1,19 +1,4 @@
 import "../styles/style.css";
-import { store } from "./shop";
-console.log(store);
-const DOMSelectors = {
-    all: document.getElementById("all"),
-    all2: document.getElementById("all2"),
-    loved: document.getElementById("featured"),
-    classics: document.getElementById("classic"),
-    modern: document.getElementById("modern"),
-    checkbox: document.querySelector('.checkbox'),
-    shop: document.querySelector('.storedisplay'),
-    featured: document.querySelector('.home'),
-    card: document.querySelector('.card'),
-    year: document.querySelector('.year'),
-    searchbar: document.getElementById("searchBar"),
-    itemimg: document.getElementsByClassName("itemimg")
-}
+
 
 
