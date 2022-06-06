28f192eb42020903d4489417a6730df3c0d4234b
williamcorwin <williamc400@nycstudents.net>
2022-06-06 13:54:31 -0400

    1234

--- my-project/js/main.js
+++ my-project/js/main.js
@@ -1,4 +1,17 @@
 import "../styles/style.css";
+import { gsap } from "gsap";
 
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(".box", {
+    scrollTrigger: {
+        trigger: ".box",
+        toggleActions: "restart none none none"
+    },
+    x:400,
+    rotation: 360,
+    duration:3
+});
 
 
