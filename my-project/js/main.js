c109bacad1ba843875c1a9d49b5f7637caba7030
williamcorwin <williamc400@nycstudents.net>
2022-06-07 13:05:04 -0400

    12

--- my-project/js/main.js
+++ my-project/js/main.js
@@ -1,17 +1,35 @@
 import "../styles/style.css";
 import { gsap } from "gsap";
-
-
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(".toptags", {
+    x:-30,
+    duration:1
+});
+
+gsap.to(".logo", {
+    x:30,
+    duration:1
+});
+
+gsap.to(".bigphoto", {
+    scale: 1.025,
+    duration:1
+});
 
 gsap.to(".mapcardimg", {
     scrollTrigger: {
         trigger: ".mapcardimg",
-        toggleActions: "restart none none none"
     },
-    x:400,
-    rotation: 360,
-    duration:3
+    y: -50,
+    duration:.5
 });
 
-
+gsap.to(".gpscrollanim", {
+    scrollTrigger: {
+        trigger: ".mapcardimg",
+    },
+    y: -50,
+    duration:.5
+});
