4c484c01bde27855d296e93bf66c71cab99e2888
williamcorwin <williamc400@nycstudents.net>
2022-06-07 20:54:33 -0400

    12345

--- my-project/js/main.js
+++ my-project/js/main.js
@@ -3,6 +3,20 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
+const DOMSelectors = {
+    checkbox: document.querySelector('.checkbox'),
+
+}
+
+DOMSelectors.checkbox.addEventListener('change', () => {
+    document.body.classList.toggle('dark');
+});
+
+ScrollTrigger.defaults({
+    scrub: "true",
+    toggleActions: "restart none none none"
+});
+
 gsap.to(".toptags", {
     x:-30,
     duration:1
@@ -15,36 +29,104 @@ gsap.to(".logo", {
 
 gsap.to(".bigphoto", {
     scrollTrigger: {
-        trigger: ".mapcardimg",
-        scrub: "true",
-        toggleActions: "restart none none none"
+        trigger: ".scrolleranim",
     },
-    y: -100,
+    y: -200,
 });
-
-gsap.to(".mapcardimg", {
+ 
+gsap.to(".scrolleranim", {
     scrollTrigger: {
-        trigger: ".mapcardimg",
-        scrub: "true",
-        toggleActions: "restart none none none"
+        trigger: ".scrolleranim",
     },
-    y: -100,
+    y: -200,
 });
-
+ 
 gsap.to(".gpscrollanim", {
     scrollTrigger: {
         trigger: ".gpscrollanim",
-        scrub: "true",
-        toggleActions: "restart none none none"
     },
-    y: -100,
+    y: -200,
 });
-
+ 
 gsap.to(".mapcontainer", {
     scrollTrigger: {
         trigger: ".mapcontainer",
-        scrub: "true",
-        toggleActions: "restart none none none"
     },
-    y: -100,
+    y: -200,
+});
+ 
+gsap.to("p1", {
+    scrollTrigger: {
+        trigger: "p1",
+    },
+    y: -200,
+});
+ 
+gsap.to(".p1", {
+    scrollTrigger: {
+        trigger: ".p1",
+    },
+    y: -200,
+});
+ 
+gsap.to(".p2", {
+    scrollTrigger: {
+        trigger: ".p2",
+    },
+    y: -200,
+});
+ 
+gsap.to(".p3", {
+    scrollTrigger: {
+        trigger: ".p3",
+    },
+    y: -200,
+});
+ 
+gsap.to(".p4", {
+    scrollTrigger: {
+        trigger: ".p4",
+    },
+    y: -200,
+});
+ 
+gsap.to(".p5", {
+    scrollTrigger: {
+        trigger: ".p5",
+    },
+    y: -200,
+});
+ 
+gsap.to(".item1", {
+    scrollTrigger: {
+        trigger: ".item1",
+    },
+    y: -200,
+});
+ 
+gsap.to(".item2", {
+    scrollTrigger: {
+        trigger: ".item2",
+    },
+    y: -200,
+});
+
+gsap.to(".right1", {
+    scrollTrigger: {
+        trigger: ".left1",
+        scrub: false,
+        toggleActions: "restart none complete none"
+    },
+    y: -5,
+    duration: 1
+});
+
+gsap.to(".left1", {
+    scrollTrigger: {
+        trigger: ".left1",
+        scrub: false,
+        toggleActions: "restart none complete none"
+    },
+    y: 5,
+    duration: 1
 });
